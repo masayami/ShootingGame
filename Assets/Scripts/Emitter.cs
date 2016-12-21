a552f616b2e2836f1a7b75105a138d565421a757
miya <vexen013@gmail.com>
2016-12-21 16:16:15 +0900

    プロジェクトを追加

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+
+	// Waveプレハブを格納する
+	public GameObject[] waves;
+
+	//現在のWave
+	private int currentWave;
+
+	private Manager manager;
+
+	IEnumerator Start(){
+
+		// Waveが存在しなければコルーチンを終了する。
+		if (waves.Length == 0) {
+			yield break;
+		}
+
+		// Managerコンポーネントをシーン内から探して取得する。
+		manager = FindObjectOfType<Manager>();
+
+		while (true) {
+
+			// タイトル表示中は待機
+			while(manager.IsPlaying() == false){
+				yield return new WaitForEndOfFrame ();
+			}
+
+			//Waveを作成
+			GameObject g = (GameObject)Instantiate (waves [currentWave], transform.position, Quaternion.identity);
+
+			// WaveをEmitterの子要素にする
+			g.transform.parent = transform;
+
+			// WaveをEnemyの子要素にする
+			//wave.transform.parent = transform;
+
+			// Waveの子要素のEnemyが全て削除されるまで待機する
+			while (g.transform.childCount != 0) {
+				yield return new WaitForEndOfFrame ();
+			}
+
+			// Waveの削除
+			Destroy (g);
+
+			// 格納されているWaveを全て実行したらcurrentwaveを0にする。(最初から -> ループ)
+			if (waves.Length <= ++currentWave) {
+				currentWave = 0;
+			}
+		}
+	}
+}
